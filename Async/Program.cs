f4f3e3674b8896b79c2e4941677afcf5e1c4457d
sanchit <sanchit.bhardwaj31@outlook.com>
2019-03-04 08:33:42 +0530

    Async example

---
+++ Async/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace async
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            AsynClass aobj = new AsynClass();
+            aobj.DoStuff();
+
+            for(int i=0;i<1000;i++)
+            {
+                Console.WriteLine("THis is from the main thread " + i);
+            }
+        }
+    }
+
+    public class AsynClass
+    {
+        public async Task DoStuff()
+        {
+            await Task.Run(() => LongRunningTask());
+        }
+
+        private static async Task LongRunningTask()
+        {
+            int counter = 0;
+            for (int i = 0; i <= 500; i++)
+            {
+                Console.WriteLine("Async class = " + i);
+                counter += 1;
+            }
+        }
+    }
+}
